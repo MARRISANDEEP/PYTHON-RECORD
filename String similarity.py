f311f6e41023bca21dc2dd9a36d11ae57ffd2043
MARRISANDEEP <11249a214@kanchiuniv.ac.in>
2025-11-20 22:07:50 +0530

    String similarity.py

---
+++ String similarity.py
@@ -0,0 +1,10 @@
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+min_len = min(len(string1), len(string2))
+max_len = max(len(string1), len(string2))
+match_count = 0
+for i in range(min_len):
+    if string1[i] == string2[i]:
+        match_count += 1
+similarity = (match_count / max_len) * 100
+print(f"String similarity: {similarity:.2f}%")
